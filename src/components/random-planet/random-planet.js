02f2b7395040e72fc35288f5f5c9a0e3a39a7b7d
Kanat Karabekov <mr.kanat2004@gmail.com>
2023-12-03 19:44:37 +0600

    refractoring code and adding components

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -14,24 +14,38 @@ export default class RandomPlanet extends Component {
         error: false
     };
 
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
+        this.interval = setInterval( this.updatePlanet, 60000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
+            loading: false,
+            error: false
+        });
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
             loading: false
         })
     };
 
-    updatePlanet() {
-        const id = Math.floor(Math.random() * 25 + 2);
-        this.swapiService.getPlanet(id)
+    updatePlanet = () => {
+        const id = Math.floor( Math.random() * 17 ) + 3;
+
+        this.swapiService
+            .getPlanet(id)
             .then(this.onPlanetLoaded)
-            .catch(this.onError)
-    }
+            .catch(this.onError);
+    };
 
     onError = (err) => {
         this.setState({
@@ -42,7 +56,7 @@ export default class RandomPlanet extends Component {
 
 
     render() {
-        const { planet, loading, error} = this.state;
+        const {planet, loading, error} = this.state;
 
         const hasData = !(loading || error);
 
@@ -67,7 +81,7 @@ const PlanetView = ({planet}) => {
     return (
         <React.Fragment>
             <img className="planet-image"
-                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet"/>
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
             <div>
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
@@ -86,5 +100,5 @@ const PlanetView = ({planet}) => {
                 </ul>
             </div>
         </React.Fragment>
-    );
+    )
 };
